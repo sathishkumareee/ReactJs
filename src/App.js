8b3de3676d31aaee5d76dd0d4570e537264a54d8
Neosoft <neosoft@Sathishs-Macbook-pro-9.local>
2025-01-08 11:32:22 +0530

    secondcommit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi Sathish
+          Hi Sathish Kumar
         </a>
       </header>
     </div>
